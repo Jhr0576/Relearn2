2507688c0f4acf5f5740693273b53abe71d3316b
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-04-20 04:20:36 +0000

    3

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,43 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+-- Create the rest of the tables
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  created_at TEXT,
+  notes TEXT,
+  salespersons_id INTEGER,
+  contacts_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
